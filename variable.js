c55b4b66ab4ccd1b2e0ffa911790ccc10fd47e19
syed-saad-1 <sfurqanarif7528@gmail.com>
2025-11-04 17:06:31 -1200

    Variables

---
+++ variable.js
@@ -0,0 +1,24 @@
+// console.log("Mr : Saad");
+
+// ( 👇 this code is Show Differnce between [Var , Let , Const 👇)
+
+var accountName = "Saad Raza "; // (prefer not use to var becouse Scope Problem Block & Function)
+
+let accountId = 234358759; // (in my point of view best use is let becouse let is not a Scope 
+// problem and this is  Rechange )
+
+const accountEmail = "syedsaad@gmail.com"; // ( and const is not abbility to exchange value )
+
+// Exchange  
+
+dccountCity = "lahore"; // this is change ability
+accountName = "Syed Furqan"; // this is change ability
+accountId = 123874; // this is change ability
+// accountEmail = "sfurqanarif7528@gmail.com"; // this is not change ability
+dccountCity = "Karachi"; // this is change ability
+
+console.log(accountName);
+console.log(accountId);
+console.log(accountEmail);
+console.log(dccountCity);
+console.table([accountName, accountId, accountEmail, dccountCity])
