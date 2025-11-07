b347b2dff3801a455ba1fd3502f07ac7608947b5
syed-saad-1 <sfurqanarif7528@gmail.com>
2025-11-06 17:24:18 -1200

    Comparision_Datatype

---
+++ Comparision_datatype.js
@@ -0,0 +1,19 @@
+// // console.log(2 <  4); //chota
+// // console.log(2 >  4); //bara 
+
+// // console.log(20 < 30);
+// // console.log(47 > 37);
+// // console.log(47 <= 37);
+// // console.log(47 >= 37);
+// // console.log(37 == 37);
+// // console.log(47 != 37);
+
+// // console.log("2" < 3);
+// // console.log("02" < 3);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log("3" == 3);
+// console.log("3" === 3); // this is Wrong
+// console.log(3 === 3); // this is Right
