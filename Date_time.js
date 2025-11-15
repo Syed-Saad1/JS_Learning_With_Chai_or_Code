b16559eaa53fe5bf8ede6c9093bf259039e5fdaa
syed-saad-1 <sfurqanarif7528@gmail.com>
2025-11-14 16:02:51 -1200

    Date & Time

---
+++ Date_time.js
@@ -0,0 +1,49 @@
+let MyDate = new Date();
+// console.log(MyDate);
+
+// console.log(MyDate.toString());         
+// (👆 This Method is Show Day , Full Date , time , and Standard)
+// console.log(MyDate.toDateString());   
+// (👆 This Method is Show Simple Day , Full Date )
+// console.log(MyDate.toISOString());    
+// (👆 This Method is Show Full Date & time with hours mint milisecond )
+// console.log(MyDate.toJSON()); 
+// (👆 This Method is Show Full Date & time with hours mint milisecond )
+// console.log(MyDate.toLocaleString()); 
+// (👆 This Method is Show FullDate in Number format and Simple Show time)
+// console.log(MyDate.toLocaleDateString());   
+// (👆 This Method is Show only FullDate in Number format)
+// console.log(MyDate.toLocaleTimeString()); 
+// (👆 This Method is Show time with Hours mint second and  time format)
+// console.log(typeof MyDate);
+
+let MyCreated = new Date(2025, 11, 1);
+console.log(MyCreated.toDateString());
+// (👆 This Method is Create a Cutomerize date)
+
+let My_MM_DD_YY = new Date("04-28-2010");
+console.log(My_MM_DD_YY.toDateString());
+// (👆 This Method is Create a Cutomerize date but format is used DD/MM/YY)
+
+let Mytime = Date.now()
+console.log(Mytime);
+// ( 👆 This Method is Show Current timeStamp in milisecond Since 1/january/1970..)
+
+console.log(new Date().getTime());
+
+let MySecond = Math.floor(Mytime / 1000);
+console.log(MySecond);
+
+let MyDate1 = new Date ( )
+console.log(MyDate1);
+console.log(MyDate1.getDate());
+console.log(MyDate1.getDay());
+console.log(MyDate1.getFullYear());
+console.log(MyDate1.getTime());
+console.log(MyDate1.getMonth() + 1);
+
+let MyJambo =  toLocaleString( 'default' ,{
+    weekday: "short",
+    second: "2-digit"
+})
+console.log(MyJambo);
