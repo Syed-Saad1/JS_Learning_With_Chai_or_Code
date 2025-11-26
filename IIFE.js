afa179c37a044f1912afe630c34d810a5a07a45f
syed-saad-1 <sfurqanarif7528@gmail.com>
2025-11-25 14:59:25 -1200

    Variable : datatype : Comparision : Convesrtion&operation : String : datatypepart2 : Number Guessing-Game : Number&Maths  :  Array_Part_1,2   : Object1 : Object2 : function : Scope : Arrow_Function : IIFE

--- IIFE.js
+++ IIFE.js
@@ -1,1 +1,16 @@
 // Immediately Invoked Function Expression  ( IIFE )
+
+function chai (){
+    console.log(`DB Connected One `);
+}
+chai();
+
+(function IIFE (){
+    // This is a named IIFE
+    console.log(`DB Connected Two`);
+})();
+
+((name) => {
+    console.log(`DB Connected Three ${name}`);
+    
+})('Saad Raza');
